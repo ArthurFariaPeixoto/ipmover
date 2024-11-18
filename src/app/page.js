06ab74751984836b9be790b57d42483c3031b7b6
imoraes <imoraes@funape.org.br>
2024-11-18 14:05:51 -0300

    asddsaasd

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Contact from "@/section/contact";
 import Footer from "@/section/footer";
 import Header from "@/section/header";
 import Services from "@/section/services";
+import StatsSection from "@/section/stats";
 import Testimonials from "@/section/testiomonials";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <Services />
 
       <Testimonials />
+      <StatsSection />
       <Contact />
       <Footer />
     </>
